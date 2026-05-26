d4ffc605713ba45911e0e10b1b3cb214ef9ec5ea
Lukáš Krajči <luky.krajci@gmail.com>
2026-05-26 16:20:17 +0200

    Refactor tests to utilize basePages fixture and streamline login and home functionalities

--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -1,38 +1,41 @@
 import test, { expect } from '../fixtures/basePages';
 
+test.describe('Home Tests', () => {     
+
+    test.beforeEach(async ({loginPage }) => {
+        await loginPage.gotoLoginPage();
 
 
-test('Verify home title', async ({ page, loginPage, homePage }) => {
-    await loginPage.gotoLoginPage();
-    await loginPage.login();
-   
-    await expect(homePage.title).toBeVisible();
+    });
+
+    test('Verify home title', async ({ page, loginPage, homePage }) => {
+            await loginPage.login();
+    
+        await expect(homePage.title).toBeVisible();
+    });
+
+    test('Verify add to cart functionality', async ({ page, loginPage, homePage }) => {
+        await loginPage.login();
+        await homePage.clickOnAddToCart();
+    
+        await expect(homePage.cartBadge).toHaveText("1");  
+    });
+
+    test('Remove item from cart', async ({ page, loginPage, homePage  }) => {
+          await loginPage.login();
+        await homePage.clickOnAddToCart();
+    
+        await expect(homePage.cartBadge).toHaveText("1");  
+        await homePage.clickOnRemoveFromCart();
+        await expect(homePage.cartBadge).not.toBeVisible();
+    });
+
+    test('Go back to products', async ({ page, loginPage, homePage }) => {
+        await loginPage.login();
+        await homePage.clickOnItem();
+
+        await homePage.clickOnBackToProducts();
+        await expect(homePage.title).toBeVisible();
+    });
+
 });
-
-test('Verify add to cart functionality', async ({ page, loginPage, homePage }) => {
-    await loginPage.gotoLoginPage();
-    await loginPage.login();
-    await homePage.clickOnAddToCart();
-   
-    await expect(homePage.cartBadge).toHaveText("1");  
-});
-
-test('Remove item from cart', async ({ page, loginPage, homePage  }) => {
-    await loginPage.gotoLoginPage();
-    await loginPage.login();
-    await homePage.clickOnAddToCart();
-   
-    await expect(homePage.cartBadge).toHaveText("1");  
-    await homePage.clickOnRemoveFromCart();
-    await expect(homePage.cartBadge).not.toBeVisible();
-});
-
-test('Go back to products', async ({ page, loginPage, homePage }) => {
-    await loginPage.gotoLoginPage();
-    await loginPage.login();
-    await homePage.clickOnItem();
-
-    await homePage.clickOnBackToProducts();
-    await expect(homePage.title).toBeVisible();
-});
-
--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,54 +1,59 @@
 import test, { expect } from '../fixtures/basePages';
 
-test('Successful login', async ({ page, loginPage }) => {
-    await loginPage.gotoLoginPage();
-    await loginPage.enterValidUsername();
-    await loginPage.enterValidPassword();
-    await loginPage.clickLoginButton();
-   
-    await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
+
+test.describe('Login Tests', () => {     
+
+    test.beforeEach(async ({loginPage }) => {
+        await loginPage.gotoLoginPage();
+
+    });
+
+    test('Successful login', async ({ page, loginPage }) => {
+        await loginPage.enterValidUsername();
+        await loginPage.enterValidPassword();
+        await loginPage.clickLoginButton();
+    
+        await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
+    });
+
+    test('Successful login_Shorter', async ({ page, loginPage }) => {
+        await loginPage.login();
+    
+        await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
+    });
+
+    test('Cannot login with valid username and invalid password', async ({ page, loginPage }) => {
+        await loginPage.enterValidUsername();
+        await loginPage.enterInvalidPassword();
+        await loginPage.clickLoginButton();
+
+        await expect(loginPage.invalidCredentialErrorMessage).toBeVisible();
+    });
+
+    test('Cannot login with invalid username and valid password', async ({ page, loginPage }) => {
+        await loginPage.enterInvalidUsername();
+        await loginPage.enterValidPassword();
+        await loginPage.clickLoginButton();
+
+        await expect(loginPage.invalidCredentialErrorMessage).toBeVisible();
+    });
+
+    test('Cannot login with blank fields', async ({ page, loginPage }) => {
+        await loginPage.clickLoginButton();
+
+        await expect(loginPage.requiredCredentialErrorMessage).toBeVisible();
+    });
+
+    test('Cannot login with locked out user', async ({ page, loginPage   }) => {
+        await loginPage.enterLockedOutUser();
+        await loginPage.enterValidPassword();
+        await loginPage.clickLoginButton();
+
+        await expect(loginPage.lockedOutUserErrorMessage).toBeVisible();
+    });
+
 });
 
-test('Successful login_Shorter', async ({ page, loginPage }) => {
-    await loginPage.gotoLoginPage();
-    await loginPage.login();
-   
-    await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
-});
-
-test('Cannot login with valid username and invalid password', async ({ page, loginPage }) => {
-    await loginPage.gotoLoginPage();
-    await loginPage.enterValidUsername();
-    await loginPage.enterInvalidPassword();
-    await loginPage.clickLoginButton();
-
-    await expect(loginPage.invalidCredentialErrorMessage).toBeVisible();
-});
-
-test('Cannot login with invalid username and valid password', async ({ page, loginPage }) => {
-    await loginPage.gotoLoginPage();
-    await loginPage.enterInvalidUsername();
-    await loginPage.enterValidPassword();
-    await loginPage.clickLoginButton();
-
-    await expect(loginPage.invalidCredentialErrorMessage).toBeVisible();
-});
-
-test('Cannot login with blank fields', async ({ page, loginPage }) => {
-    await loginPage.gotoLoginPage();
-    await loginPage.clickLoginButton();
-
-    await expect(loginPage.requiredCredentialErrorMessage).toBeVisible();
-});
-
-test('Cannot login with locked out user', async ({ page, loginPage   }) => {
-    await loginPage.gotoLoginPage();
-    await loginPage.enterLockedOutUser();
-    await loginPage.enterValidPassword();
-    await loginPage.clickLoginButton();
-
-    await expect(loginPage.lockedOutUserErrorMessage).toBeVisible();
-});
 
 
 
