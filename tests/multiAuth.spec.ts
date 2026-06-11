2a507a42196a8612af979fe6ec012f201bf6b15f
Lukáš Krajči <luky.krajci@gmail.com>
2026-06-11 16:23:36 +0200

    fix: remove .only from user and admin login tests to run all tests

--- tests/multiAuth.spec.ts
+++ tests/multiAuth.spec.ts
@@ -4,7 +4,7 @@ import { test, expect } from '@playwright/test';
 test.use({ storageState: '.auth/user.json' });
 test.describe('User Authentication', () => {
 
-    test.only('Verify user is logged in', async ({ page }) => {
+    test('Verify user is logged in', async ({ page }) => {
         await page.goto('https://demoqa.com/login');
         await page.getByText('lukas').isVisible();
         expect(page.getByRole('button', { name: 'Log out' })).toBeVisible();
@@ -14,7 +14,7 @@ test.describe('User Authentication', () => {
 test.use({ storageState: '.auth/admin.json' });
 test.describe('Admin Authentication', () => {
 
-    test.only('Verify admin is logged in', async ({ page }) => {
+    test('Verify admin is logged in', async ({ page }) => {
         await page.goto('https://demoqa.com/login');
         await page.getByText('lukas_admin').isVisible();
         expect(page.getByRole('button', { name: 'Log out' })).toBeVisible();
