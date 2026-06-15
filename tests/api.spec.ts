4b3d70bc18a1bfab1cadf89880d825eece3bb229
Lukáš Krajči <luky.krajci@gmail.com>
2026-06-15 17:49:27 +0200

    Add new test results and attachments for API tests
    - Created new result JSON files for GET requests with parameters and API tests, including status updates (passed, skipped).
    - Added attachment files containing booking IDs and trace information for debugging.
    - Included empty attachment files for consistency in test reporting.

--- tests/api.spec.ts
+++ tests/api.spec.ts
@@ -2,13 +2,31 @@ import { test, expect } from '@playwright/test';
 
 test.describe('API Tests', () => {
 
-    test('GET request @api', async ({ request }) => {
+    test.skip('GET request @api', async ({ request }) => {
         const response = await request.get('https://restful-booker.herokuapp.com/booking');
         expect(response.status()).toBe(200);
 
         const responseBody = await response.json();
         console.log(JSON.stringify(responseBody)); 
     });
+
+
+ test('GET request with parameters @api', async ({ request }) => {
+        const response = await request.get('/booking', {
+            params: {
+                firstname: 'Jane',
+                lastname: 'Doe'
+            }
+        });
+        expect(response.ok()).toBeTruthy();
+        expect(response.status()).toBe(200);
+        console.log(await response.json());
+    });
+
+
+
+
+
 
 });
 
