50cdaf4da1eb4fbec22c363d5f95c554c124c3ba
Lukáš Krajči <luky.krajci@gmail.com>
2026-06-16 15:29:20 +0200

    Add API test results and attachments for booking creation
    - Created new result JSON files for POST request - create booking and GET request with parameters.
    - Added attachments for booking details in text and markdown formats.
    - Updated allure results with detailed test execution information and error handling.
    - Included trace files for debugging failed tests.

--- tests/api.spec.ts
+++ tests/api.spec.ts
@@ -23,10 +23,29 @@ test.describe('API Tests', () => {
         console.log(await response.json());
     });
 
+    test.only('POST request - create booking @api', async ({ request }) => {
+        const response = await request.post('/booking', {
+            data: {
+                "firstname" : 'Lukas',
+                "lastname" : 'Priezvisko',
+                "totalprice" : 555,
+                "depositpaid" : true,
+                "bookingdates" : {
+                    "checkin" : "2026-05-06",
+                    "checkout" : "2026-05-09"
+                },
+                "additionalneeds" : "Breakfast, Lunch"
+            }
+        });
+        expect(response.ok()).toBeTruthy();
+        expect(response.status()).toBe(200);
+        console.log(await response.json());
 
-
-
-
+        const responseBody = await response.json();
+        expect(responseBody.booking).toHaveProperty('firstname', 'Lukas');
+        expect(responseBody.booking).toHaveProperty('lastname', 'Priezvisko');
+        expect(responseBody.booking).toHaveProperty('totalprice', 555);
+        expect(responseBody.booking).toHaveProperty('depositpaid', true);
+    });
 
 });
-
